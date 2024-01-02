5110d13dfcfff5ad0e7bb2e746f0d967e5ff7163
CSharplie <charlie.collier@outlook.fr>
2024-01-02 21:45:07 +0100

    Add MSSQL connector

--- src/ATF/connectors/connector_mssql.py
+++ src/ATF/connectors/connector_mssql.py
@@ -2,6 +2,7 @@
 """Connector to read MSSQL database"""
 
 import pandas as pd
+from sqlalchemy import create_engine
 from connectors.connector import Connector
 
 class ConnectorMSSQL(Connector):
@@ -12,9 +13,9 @@ class ConnectorMSSQL(Connector):
         self.connection_definition = [
             {
                 "name": "mode",
-                "validset": ["connection_string", "password"],
-                "default" : "connection_string"
-            },
+                "default" : "password",
+                "validset": ["password", "connection_string"]
+            },    
             {
                 "name": "hostname",
                 "default" : None
@@ -32,15 +33,37 @@ class ConnectorMSSQL(Connector):
                 "default" : None
             },
             {
-                "name": "driver",
-                "default" : "SQL Server"
+                "name":"port",
+                "default": 1433,
+                "type": "integer"
+            },
+            {
+                "name":"driver",
+                "default": "ODBC Driver 18 for SQL Server",
+            },
+            {
+                "name":"connection_string",
+                "default": None
             }
         ]
-        self.configuration_definition = [{ "name": "query" }]
+        self.configuration_definition = [{ "name": "query" }, { "name": "connection" }]
 
     def get_data(self, configuration: dict, connection: dict):
         """Get data from source"""
 
-        path = configuration["path"]
-        df = pd.read_csv(path)
+        connection_string = connection["connection_string"]
+        if connection["mode"] == "password":
+            driver = connection["driver"]
+            port = connection["port"]
+            hostname = connection["hostname"]
+            username = connection["username"]
+            password = connection["password"]
+            database = connection["database"]
+
+            connection_string = f"mssql+pyodbc://{username}:{password}@{hostname}:{port}/{database}?driver={driver}"
+
+        sql_connection = create_engine(connection_string, echo=False)
+
+        df = pd.read_sql(configuration["query"], sql_connection)
+
         return df
