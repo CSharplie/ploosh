71e0e6b9cd3230fbb55c11e7abf30e7b31dc52aa
CSharplie <charlie.collier@outlook.fr>
2024-01-27 15:42:42 +0100

    Add dependecies and entry point

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,28 @@ setup (
     name = "ATF",
     version = "0.0.1",
     platforms="Any",
+    long_description="tests",
     python_requires=">=3.6",
     license= "Apache License 2.0",
     packages = find_packages(),
+    entry_points = {
+        "console_scripts": [
+            "ATF = ATF.__main__:main"
+        ]
+    },
+    install_requires=[
+        "colorama==0.4.6",
+        "PyYAML==6.0.1",
+        "Pyjeb==0.2.1",
+        "pandas==2.1.4",
+        "openpyxl==3.1.2",
+        "sqlalchemy==1.4.51",
+        "pyodbc==5.0.1",
+        "pymysql==1.1.0",
+        "pg8000==1.30.3",
+        "snowflake-sqlalchemy==1.5.1",
+        "databricks-sql-connector==2.9.3",
+        "sqlalchemy-bigquery==1.9.0",
+        "google-cloud-bigquery-storage==2.24.0",
+    ],
 )
