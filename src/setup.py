82038e851e35da495546f65cbbc0c0a0dfb68804
CSharplie <charlie.collier@outlook.fr>
2024-01-27 17:11:51 +0100

    rename to ploosh

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ setup (
     packages = find_packages(),
     entry_points = {
         "console_scripts": [
-            "ploosh = ATF.__main__:main"
+            "ploosh = ploosh.__main__:main"
         ]
     },
     install_requires=[
