fe6b1b36028f48ad6dacfb28b81b8de9f5a0355e
CSharplie <charlie.collier@outlook.fr>
2024-02-05 17:46:57 +0100

    Init setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,31 @@
 """Setup PyPi module"""
+# pylint: disable=C0103
+
 from setuptools import setup, find_packages
+
+with open("../README.md", encoding="UTF-8") as f:
+    long_description = "".join(f.readlines())
+
+# replace relative link by absolute github link
+long_description = long_description.replace("(/", "(https://github.com/CSharplie/ploosh/blob/main/")
 
 setup (
     name = "ploosh",
     version = "0.1.0",
+    description="A framework to automatize your tests for data projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CSharplie/ploosh/",
+    project_urls={
+        "Bug Tracker": "https://github.com/CSharplie/ploosh/issues",
+        "CI": "https://github.com/CSharplie/ploosh/actions",
+        "Documentation": "https://github.com/CSharplie/ploosh",
+        "Source Code": "https://github.com/CSharplie/ploosh",
+    },
+    download_url="https://pypi.org/project/ploosh/",
     platforms="Any",
-    long_description="tests",
     python_requires=">=3.6",
     license= "Apache License 2.0",
-    packages = find_packages(),
     entry_points = {
         "console_scripts": [
             "ploosh = ploosh.__main__:main"
