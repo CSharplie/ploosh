3956fce3df1072302a57067799a9d527d575e24e
CSharplie <charlie.collier@outlook.fr>
2024-02-06 16:46:23 +0100

    Split requirements

--- src/setup-core.py
+++ src/setup-core.py
@@ -1,3 +1,3 @@
 from setup import setup_ploosh
 
-setup_ploosh("ploosh-core")
+setup_ploosh("ploosh-core", [])
--- src/setup.py
+++ src/setup.py
@@ -3,16 +3,27 @@
 
 from setuptools import setup, find_packages
 
-def setup_ploosh(name):
+PLOOSH_VERSION = "0.1.10"
+
+def setup_ploosh(name, install_requires):
     with open("../readme.md", encoding="UTF-8") as f:
         long_description = "".join(f.readlines())
 
     # replace relative link by absolute github link
     long_description = long_description.replace("(/", "(https://github.com/CSharplie/ploosh/blob/main/")
 
+    install_requires = install_requires + [
+            "colorama==0.4.6",
+            "PyYAML==6.0.1",
+            "Pyjeb==0.2.1",
+            "pandas==2.1.4",
+            "openpyxl==3.1.2",
+            "sqlalchemy==1.4.51"
+        ]
+
     setup (
         name = name,
-        version = "0.1.9",
+        version = PLOOSH_VERSION,
         description="A framework to automatize your tests for data projects",
         long_description=long_description,
         long_description_content_type="text/markdown",
@@ -32,19 +43,5 @@ def setup_ploosh(name):
                 "ploosh = ploosh.__main__:main"
             ]
         },
-        install_requires=[
-            "colorama==0.4.6",
-            "PyYAML==6.0.1",
-            "Pyjeb==0.2.1",
-            "pandas==2.1.4",
-            "openpyxl==3.1.2",
-            "sqlalchemy==1.4.51",
-            "pyodbc==5.0.1",
-            "pymysql==1.1.0",
-            "pg8000==1.30.3",
-            "snowflake-sqlalchemy==1.5.1",
-            "databricks-sql-connector==2.9.3",
-            "sqlalchemy-bigquery==1.9.0",
-            "google-cloud-bigquery-storage==2.24.0",
-        ],
+        install_requires=install_requires,
     )
