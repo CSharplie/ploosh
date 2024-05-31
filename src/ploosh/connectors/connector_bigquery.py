be41c5d376c634d229cb7b59d16e619e7be50ecf
CSharplie <charlie.collier@outlook.fr>
2024-05-31 12:56:26 +0200

    Allow current_user parameter

--- src/ploosh/connectors/connector_bigquery.py
+++ src/ploosh/connectors/connector_bigquery.py
@@ -2,6 +2,7 @@
 """Connector to read BigQuery database"""
 
 import pandas as pd
+import pandas_gbq
 from sqlalchemy import create_engine
 from connectors.connector import Connector
 
@@ -12,19 +13,34 @@ class ConnectorBigQuery(Connector):
         self.name = "BIGQUERY"
         self.connection_definition = [
             {
-                "name": "credentials"
+                "name": "credentials",
+                "default": None
             },
+            {
+                "name": "credentials_type",
+                "validset": ["service_account", "current_user"],
+                "default": "service_account"
+            },
+            {
+                "name": "project_id",
+                "default": None
+            }
         ]
-        self.configuration_definition = [{ "name": "query" }, { "name": "connection" }]
+        self.configuration_definition = [
+            { "name": "query" }
+            , { "name": "connection" }
+        ]
 
     def get_data(self, configuration: dict, connection: dict):
         """Get data from source"""
-
         credentials = connection["credentials"]
-        connection_string = f"bigquery://?credentials_base64={credentials}"
+        credentials_type = connection["credentials_type"]
 
-        sql_connection = create_engine(connection_string, echo=False)
-
-        df = pd.read_sql(configuration["query"], sql_connection)
+        if credentials_type == "service_account":
+            connection_string = f"bigquery://?credentials_base64={credentials}"
+            sql_connection = create_engine(connection_string, echo=False)
+            df = pd.read_sql(configuration["query"], sql_connection)
+        elif credentials_type == "current_user":
+            df = pandas_gbq.read_gbq(configuration["query"], connection["project_id"], progress_bar_type = None)
 
         return df
