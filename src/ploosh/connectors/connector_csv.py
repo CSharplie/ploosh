1f3073ff272a77e48b2c4dfb8dbcf7309b340813
CSharplie <charlie.collier@outlook.fr>
2024-09-12 10:11:40 +0000

    Restore CSV connector

--- src/ploosh/connectors/connector_csv.py
+++ src/ploosh/connectors/connector_csv.py
@@ -1,31 +1,29 @@
 # pylint: disable=R0903
 """Connector to read CSV file"""
 
+import pandas as pd
 from connectors.connector import Connector
 
 
-class ConnectorCSVSpark(Connector):
-    """Connector to read CSV file with Spark"""
+class ConnectorCSV(Connector):
+    """Connector to read CSV file"""
 
     def __init__(self):
         # Initialize the connector with its name and configuration definitions
-        self.name = "CSV_SPARK"
-        self.is_spark = True
+        self.name = "CSV"
         self.connection_definition = []  # No specific connection parameters required
         self.configuration_definition = [
             {"name": "path"},  # Path to the CSV file
             {"name": "delimiter", "default": ","},  # Delimiter used in the CSV file
-            {"name": "header", "type": "boolean", "default": True},  # Whether the CSV file has a header row
         ]
 
     def get_data(self, configuration: dict, connection: dict):
         """Get data from source"""
 
-        # Read the CSV file using Spark with the specified configuration options
-        df = (
-            self.spark.read.option("delimiter", configuration["delimiter"])
-            .option("header", configuration["header"])
-            .csv(configuration["path"])
-        )
+        # Extract the path and delimiter from the configuration
+        path = configuration["path"]
+        delimiter = configuration["delimiter"]
 
+        # Read the CSV file using pandas with the specified delimiter
+        df = pd.read_csv(path, delimiter=delimiter)
         return df
