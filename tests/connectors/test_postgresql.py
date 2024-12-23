28cd14e962d37a9924976c731704815c2069390b
CSharplie <charlie.collier@outlook.fr>
2024-12-23 21:55:50 +0000

    Init postgresql tests

--- tests/connectors/test_postgresql.py
+++ tests/connectors/test_postgresql.py
@@ -1,33 +1,51 @@
 import os
 import pandas as pd
+import pytest
 from pyjeb import control_and_setup
 from ploosh.connectors.connector_postgresql import ConnectorPostgreSQL
 
-connector = ConnectorPostgreSQL()
+@pytest.fixture
+def connector():
+    return ConnectorPostgreSQL()
 
-connection_password = {
-    "hostname": "localhost",
-    "username": "ploosh",
-    "password": os.environ.get("TEST_DB_PASSWORD"),
-    "database": "ploosh"
-}
+@pytest.fixture
+def df_sales():
+    return pd.read_csv("./tests/.data/sales.csv", delimiter=",", date_format = "%Y-%m-%d", parse_dates=["sale_date"])
 
-control_and_setup(connection_password, connector.connection_definition)
 
-df_sales = pd.read_csv("./tests/.env/csv/sales.csv", delimiter=",")
-
-def test_connection_with_password():
+def test_connection_with_password(connector, df_sales):
     configuration = {
         "query": "select * from sales;",
         "connection": "debug"
     }
 
-    control_and_setup(configuration, connector.configuration_definition)
+    connection = {
+        "hostname": "localhost",
+        "username": "ploosh",
+        "password": os.environ.get("TEST_DB_PASSWORD"),
+        "database": "ploosh"
+    }
+    connection = control_and_setup(connection, connector.connection_definition)
 
-    df_test = connector.get_data(configuration, connection_password)
+    df_test = connector.get_data(configuration, connection)
 
-    
+    assert len(df_test.compare(df_sales)) == 0
 
-    assert df_test.compare(df_sales)
+def test_connection_with_connection_string(connector, df_sales):
+    configuration = {
+        "query": "select * from sales;",
+        "connection": "debug"
+    }
+
+    connection = {
+        "mode": "connection_string",
+        "connection_string": f"postgresql+pg8000://ploosh:{os.environ.get("TEST_DB_PASSWORD")}@localhost/ploosh"
+    }
+
+    connection = control_and_setup(connection, connector.connection_definition)
+
+    df_test = connector.get_data(configuration, connection)
+
+    assert len(df_test.compare(df_sales)) == 0
 
      
