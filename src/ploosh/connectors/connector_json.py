d9e5592146120a79043a5bc0c0e0764821764906
bileljedda <bilel.jedda.perso@gmail.com>
2025-01-12 19:51:39 +0100

    Add new JSON natif connector

--- src/ploosh/connectors/connector_json.py
+++ src/ploosh/connectors/connector_json.py
@@ -1,7 +1,6 @@
 # pylint: disable=R0903
 """Connector to read JSON file"""
 
-import json
 import pandas as pd
 from connectors.connector import Connector
 
@@ -14,34 +13,19 @@ class ConnectorJSON(Connector):
         self.name = "JSON"
         self.connection_definition = []  # No specific connection parameters required
         self.configuration_definition = [
-            {"name": "path"},  # Path to the CSV file
-            {"name": "dtype", "type": "boolean", "default": None},  #  infer dtypes
-            {"name": "encoding", "type": "string", "default": "utf-8"},  # Encoding to use for UTF when reading/writing.
-            {"name": "lines", "type": "boolean", "default": False},  # Read the file as a json object per line.
-            {"name": "nrows", "type": "integer", "default": None}  # number of lines from the line-delimited jsonfile that has to be read.
+            {"name": "path"},  # Path to the JSON file
+            {"name": "encoding", "type": "string", "default": "utf-8"},  # Encoding to use when reading the JSON file.
+            {"name": "lines", "type": "boolean", "default": False},  # Whether to treat the file as line-delimited JSON (one JSON object per line).
+            {"name": "nrows", "type": "integer", "default": None}  # Number of lines to read from a line-delimited JSON file.
         ]
 
     def get_data(self, configuration: dict, connection: dict):
         """Get data from source"""
 
-        # Extract the path and delimiter from the configuration
-        path = configuration["path"]
-        dtype = configuration["dtype"]
-        encoding = configuration["encoding"]
-        lines = configuration["lines"]
-        nrows = configuration["nrows"]
-
-        # Read the CSV file using pandas with the specified delimiter
-        df = pd.read_csv(path,
-                         delimiter=delimiter,
-                         header=header,
-                         names=names,
-                         usecols=usecols,
-                         skiprows=skiprows,
-                         skipfooter=skipfooter,
-                         nrows=nrows,
-                         lineterminator=lineterminator,
-                         quotechar=quotechar,
-                         engine=engine,
-                         encoding=encoding)
+        # Read the JSON file using pandas with the specified delimiter
+        df = pd.read_json(configuration["path"],
+                         encoding = configuration["encoding"],
+                         lines = configuration["lines"],
+                         nrows = configuration["nrows"]
+                         )
         return df
