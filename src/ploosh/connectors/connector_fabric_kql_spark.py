74e286b75d7712f36ce4e5755b86f12555a8e925
CSharplie <charlie.collier@outlook.fr>
2026-01-14 16:19:39 +0000

    Fix connector naming

--- src/ploosh/connectors/connector_fabric_kql_spark.py
+++ src/ploosh/connectors/connector_fabric_kql_spark.py
@@ -1,15 +1,15 @@
 # pylint: disable=R0903
-"""Connector to read Delta table"""
+"""Connector to read Fabric KQL data with Spark"""
 
 from connectors.connector import Connector
 
 
-class ConnectorDeltaSpark(Connector):
-    """Connector to read Delta table with Spark"""
+class ConnectorFabricKqlSpark(Connector):
+    """Connector to read Fabric KQL data with Spark"""
 
     def __init__(self):
         # Initialize the connector with its name and configuration definitions
-        self.name = "DELTA_SPARK"
+        self.name = "FABRIC_KQL_SPARK"
         self.is_spark = True  # Indicates that this connector uses Spark
         self.connection_definition = [
             {
--- src/ploosh/version.py
+++ src/ploosh/version.py
@@ -1,3 +1,3 @@
 """Current version of ploosh"""
 
-PLOOSH_VERSION = "0.3.9"
+PLOOSH_VERSION = "0.4.2"
